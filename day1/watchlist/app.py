6d0aa02a088fcce4b4ca221428c9066d0bc23b00
hua <1356284534@qq.com>
2020-04-01 16:53:30 +0800

    flask提交

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask,url_for
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello，Flask</h1>"
+    return "<h1>Hello,Flask 中国<h1>"
+
+#动态url
+@app.route('/index/<name>')
+def home(name):
+    print(url_for("home",name="Bruce"))
+    return "<h1>Hello,%s<h1>"%name
+
