61449925aeedcb1cc515dc0ecfd9931a2bbc0a56
hua <1356284534@qq.com>
2020-04-07 11:47:57 +0800

    上传到Pythonanywhere

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -27,6 +27,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(app.root_path,'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #关闭了对模型修改的监控
 app.config['SECRET_KEY'] = 'watchlist_dev'
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 #初始化扩展，传入程序实例app 在配置之后
 db = SQLAlchemy(app)
 
