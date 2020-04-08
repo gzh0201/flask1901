0bc418f0cecd50e9e505d221662f9d735d76c68c
hua <1356284534@qq.com>
2020-04-08 18:02:59 +0800

    项目上线

--- day1/watchlist/watchlistapp/__init__.py
+++ day1/watchlist/watchlistapp/__init__.py
@@ -12,9 +12,11 @@ if WIN:
 else:
     prefix = 'sqlite:////'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path),'data.db')
+# app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path),'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'watchlist_dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 
 db = SQLAlchemy(app)  # 初始化扩展，传入程序实例app
 
