1d953579cbda31f237cf2f79452d47a1e973bf1a
hua <1356284534@qq.com>
2020-04-16 17:49:00 +0800

    轮播图自动轮播

--- simple/src/main.js
+++ simple/src/main.js
@@ -1,9 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Base from './Base.vue'
+import IndexPage from './components/index.vue'
+import mock from './mock/mock'
+
 // import HelloComponent from './HelloComponent.vue'
+
+Vue.use(VueRouter)
+let router = new VueRouter({
+  mode:'history',
+  routes:[
+    {
+      path:'/',
+      component:IndexPage
+    }
+  ]
+})
 
 new Vue({
   el: '#app',
+  router,
   components:{
     Base,
     // HelloComponent
